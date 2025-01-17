bed91aeaab76ed1c3c3e6e0460a8395def72c44f
Brook  Worku Alemu <109297876+BrookW-83@users.noreply.github.com>
2025-01-17 22:31:02 +0300

    chore: Add CredentialsProvider for authentication
    This commit adds the CredentialsProvider to the authentication options.

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,16 +1,53 @@
 import NextAuth from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  // Google Provider
   providers: [
     GoogleProvider({
       clientId: process.env.Google_Client_ID as string,
       clientSecret: process.env.Google_Client_Secret as string,
     }),
-    // ...add more providers here
+
+    // Credentials Provider
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "Your Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Your password",
+        },
+      },
+      authorize: async (credentials, _) => {
+        const res = await fetch(``, {
+          method: 'POST',
+          headers: {},
+          body: JSON.stringify(
+            {
+              email: credentials?.email,
+              password: credentials?.password,
+            }
+          ),
+        })
+        const jsonResponse = await res.json()
+
+        if (res.ok && jsonResponse.success) {
+          return jsonResponse.data
+        }
+        else {
+          throw new Error(jsonResponse.message);
+        }
+      },
+    }),
   ],
 };
 
