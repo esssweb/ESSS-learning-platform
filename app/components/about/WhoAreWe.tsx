153b4d4e01563432474959da158feb35beeac767
Brook  Worku Alemu <109297876+BrookW-83@users.noreply.github.com>
2025-08-26 13:48:09 +0300

    chore: Fix responsiveness on hero section

--- app/components/about/WhoAreWe.tsx
+++ app/components/about/WhoAreWe.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
-import styles from './About.module.css';
+import Image from "next/image";
+import styles from "./About.module.css";
 
 export default function WhoAreWe() {
   return (
-    <div className="flex flex-col bg-Tertiary px-4 h-screen sm:px-5 md:px-10 lg:flex-row items-center">
+    <div className="flex flex-col bg-Tertiary px-4 h-40vh sm:px-5 md:px-10 lg:flex-row items-center">
       <div
         className={`flex-col lg:flex-1 leading-none text-2xl text-center md:text-3xl lg:text-4xl lg:text-start text-Primary font-AbolitionTestRegular ${styles.lineHeight}`}
       >
@@ -11,7 +11,7 @@ export default function WhoAreWe() {
         <p className={styles.whoAreWeText}>ARE</p>
         <p className={styles.whoAreWeText}>WE?</p>
       </div>
-      <div className="flex lg:w-3/4 mt-4 pt-4 justify-center items-start lg:pt-1 lg:justify-start lg:px-7">
+      <div className="flex lg:w-3/4 mt-2 pt-4 justify-center items-start lg:pt-1 lg:justify-start lg:px-7">
         <div className="flex flex-col text-center justify-center font-SofiaProMediumItalic text-Primary text-lg md:text-xl">
           <div className="flex flex-col lg:pl-10 pb-5 space-y-4">
             <Image
@@ -43,6 +43,5 @@ export default function WhoAreWe() {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
